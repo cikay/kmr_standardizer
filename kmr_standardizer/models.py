9b87aa4c6bd2d9c0479568a0c9af0e3d1263d1c9
Muzaffer Cıkay <muzaffercky@gmail.com>
2026-03-12 19:19:24 +0300

    Add tests

--- kmr_standardizer/models.py
+++ kmr_standardizer/models.py
@@ -4,17 +4,10 @@ from enum import Enum
 
 
 @dataclass
-class Example:
-    input: str
-    output: str
-
-
-@dataclass
 class Rule:
     pattern: str
     replacement: str
     category: str
-    examples: list[Example]
 
 
 class CategoryTypes(str, Enum):
